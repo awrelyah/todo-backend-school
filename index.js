bb2392d23a87cbb891da79c848ff7f2678966c26
awrelyah <aureliamihkk@gmail.com>
2023-01-11 14:04:29 +0200

    save data to files

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const fs = require("fs");
 
 app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 /**
  * JSDOC
  * @typedef Task
  * @property {string} name
- * @property {string} completed
+ * @property {bool} completed
  */
 
 /**
  * @type Task[]
  */
-let tasks = [];
-
-let lastTaskId = 0;
+let tasks = readJsonFile("tasks.json", []);
+let lastIDs = readJsonFile("lastIDs.json", {
+  lastTaskId: 0,
+  lastUserId: 0,
+});
 
 app.get("/", (req, res) => {
   res.send("api doc");
@@ -28,9 +31,11 @@ app.get("/tasks", (req, res) => {
 });
 
 app.post("/tasks", (req, res) => {
-  req.body.id = ++lastTaskId;
+  req.body.id = ++lastIDs.lastTaskId;
+  //tasks.push(req.body);
   res.send(req.body);
-  tasks.push(req.body);
+  writeJsonFile("tasks.json", tasks);
+  writeJsonFile("lastIDs.json", lastIDs);
 });
 
 app.put("/tasks/:id", (req, res) => {
@@ -50,6 +55,7 @@ app.put("/tasks/:id", (req, res) => {
     task[key] = req.body[key];
   }
   res.send(task);
+  writeJsonFile("tasks.json", tasks);
 });
 
 app.delete("/tasks/:id", (req, res) => {
@@ -57,8 +63,27 @@ app.delete("/tasks/:id", (req, res) => {
   let index = tasks.findIndex((task) => task.id == req.params.id);
   if (index != -1) tasks.splice(index, 1);
   res.status(204).send();
+  writeJsonFile("tasks.json", tasks);
 });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+function readJsonFile(fileName, defaultValue) {
+  try {
+    let data = fs.readFileSync("data/" + fileName, "utf8");
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(err);
+    return defaultValue;
+  }
+}
+
+function writeJsonFile(fileName, data) {
+  try {
+    fs.writeFileSync("data/" + fileName, JSON.stringify(data));
+  } catch (err) {
+    console.error(err, data);
+  }
+}
